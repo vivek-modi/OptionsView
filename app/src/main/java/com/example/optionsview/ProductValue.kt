3647a9853af4cfe1415763b0d80d04b54d001353
Vivek Modi <vivekmodi456@gmail.com>
2022-03-02 11:42:51 +0000

    update to add value is digit

--- app/src/main/java/com/example/optionsview/ProductValue.kt
+++ app/src/main/java/com/example/optionsview/ProductValue.kt
@@ -1,7 +1,17 @@
 package com.example.optionsview
 
+import androidx.core.text.isDigitsOnly
+
 data class ProductValue(
     val value: String? = null
 ) {
-    val valueInDouble = value?.toDouble()
+    val valueInDouble by lazy {
+        if (!value.isNullOrEmpty() && isDigit(value)) {
+            value.toDouble()
+        } else {
+            0.0
+        }
+    }
+
+    private fun isDigit(str: String?) = str?.isDigitsOnly()?.let { true } ?: false
 }
